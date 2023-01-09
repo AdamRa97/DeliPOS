da1d3e3ce1418d61a29819cbc28714607f9be00b
JustinDang97 <48815386+JustinDang97@users.noreply.github.com>
2023-01-09 15:21:17 -0500

    Update Hoagie.java

--- Hoagie.java
+++ Hoagie.java
@@ -1,9 +1,57 @@
-
 public class Hoagie {
 
+	private String size;
+	private String bread;
+	private String meat;
+	private String cheese;
+	private String vegatables;
+	private String condiments;
+	private String seasoning;
+
+	
+	public Hoagie(String size,String meat, String bread, String condiments, String seasoning, String cheese, String vegatables) {
+		this.size = size;
+		this.bread = bread;
+		this.meat = meat;
+		this.cheese = cheese;
+		this.condiments = condiments;
+		this.vegatables = vegatables;
+		this.seasoning = seasoning;
+	}
+	
+	//getters
+	 public String getSize() {
+	    	return size;
+	    }
+	 public String geBread() {
+	    	return bread;
+	    }
+	 public String getMeat() {
+	    	return meat;
+	    }
+	 public String getCheese() {
+		 return cheese;
+	 }
+	 public String getCondiments() {
+	    	return condiments;
+	    }
+	 public String getVegatables() {
+	    	return vegatables;
+	    }
+	 public String getSeasoning() {
+
+	    	return seasoning;
+	    }
+	 
+	 //Setters
+	 public void setSize() {
+		 
+	 }
+	 
 	public static void main(String[] args) {
 		
-		
+
+			
 	}
 
 }
